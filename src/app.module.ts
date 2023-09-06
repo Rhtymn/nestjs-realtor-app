0a01a198782895cea649549e1bdfdd9753018340
Roihan <hroihan6@gmail.com>
2023-09-06 18:56:15 +0700

    Use rate limitter

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,18 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { HomeModule } from './home/home.module';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_INTERCEPTOR, APP_GUARD } from '@nestjs/core';
 import { UserInterceptor } from './user/user/user.interceptor';
 import { AuthGuard } from './guards/auth.guard';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 
 @Module({
-  imports: [UserModule, PrismaModule, HomeModule],
+  imports: [
+    ThrottlerModule.forRoot({ ttl: 10, limit: 2 }),
+    UserModule,
+    PrismaModule,
+    HomeModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
@@ -20,6 +26,10 @@ import { AuthGuard } from './guards/auth.guard';
     {
       provide: 'APP_GUARD',
       useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
     },
   ],
 })
